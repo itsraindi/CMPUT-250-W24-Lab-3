8aa278cfdf527e7196c6d55bd7a1fa2cb7b2ca65
Alan Yu <alanrain205@gmail.com>
2026-01-23 01:41:57 -0700

    Changed Sprite, Changed Text

--- CMPUT 250 W24 Lab 3/Assets/DVDLogo.cs
+++ CMPUT 250 W24 Lab 3/Assets/DVDLogo.cs
@@ -4,61 +4,74 @@ using UnityEngine;
 
 public class DVDLogo : MonoBehaviour
 {
-    //Speed it moves at
-    public float speed = 3;
+    // Speed it moves at (✅ faster)
+    public float speed = 80f;
 
-    //Bounds of the screen (could get these with camera bounds but we can do this since it's a fixed camera)
-    public float X_Max = 5, Y_Max = 4;
+    // Bounds of the screen
+    public float X_Max = 3, Y_Max = 2;
 
-    //Current direction
+    // Current direction
     private Vector3 direction;
 
-    // Start is called before the first frame update
+    // For color change on bounce
+    private SpriteRenderer sr;
+
     void Start()
     {
-        //Randomly initialize direction
-        direction = new Vector3(Random.Range(-1f,1f), Random.Range(-1f,1f));
-        direction.Normalize();
+        // grab SpriteRenderer 
+        sr = GetComponent<SpriteRenderer>();
 
-    }
-
-    private void FlipDirectionX(){
-        direction.x*=-1;
-        direction.x+= Random.Range(-0.1f,0.1f);
-        direction.y+= Random.Range(-0.1f,0.1f);
+        // Randomly initialize direction
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
         direction.Normalize();
     }
 
-    private void FlipDirectionY(){
-        direction.y*=-1;
-        direction.x+= Random.Range(-0.1f,0.1f);
-        direction.y+= Random.Range(-0.1f,0.1f);
-        direction.Normalize();
+    private void RandomizeColor()
+    {
+        // change color if we have a SpriteRenderer
+        if (sr != null)
+        {
+            sr.color = Random.ColorHSV(0f, 1f, 0.8f, 1f, 0.8f, 1f);
+        }
     }
 
-    // Update is called once per frame
+    private void FlipDirectionX()
+    {
+        direction.x *= -1;
+        direction.x += Random.Range(-0.1f, 0.1f);
+        direction.y += Random.Range(-0.1f, 0.1f);
+        direction.Normalize();
+
+        // color change on bounce
+        RandomizeColor();
+    }
+
+    private void FlipDirectionY()
+    {
+        direction.y *= -1;
+        direction.x += Random.Range(-0.1f, 0.1f);
+        direction.y += Random.Range(-0.1f, 0.1f);
+        direction.Normalize();
+
+        // color change on bounce
+        RandomizeColor();
+    }
+
     void Update()
     {
-        //Move in direction unless we'd go out of bounds, if so bounce with some randomness
+        Vector3 newPosition = transform.position + direction * Time.deltaTime * speed;
 
-        Vector3 newPosition = transform.position + direction*Time.deltaTime*speed;
-
-        //See if a bounce needs to happen before moving
-        if (newPosition.x>X_Max){
-            FlipDirectionX();
-            
-        }
-        else if (newPosition.x<-1*X_Max){
+        // Bounce check
+        if (newPosition.x > X_Max || newPosition.x < -X_Max)
+        {
             FlipDirectionX();
         }
 
-        if (newPosition.y>Y_Max){
-            FlipDirectionY();
-        }
-        else if (newPosition.y<-1*Y_Max){
+        if (newPosition.y > Y_Max || newPosition.y < -Y_Max)
+        {
             FlipDirectionY();
         }
 
-        transform.position += direction*Time.deltaTime*speed;
+        transform.position += direction * Time.deltaTime * speed;
     }
 }
